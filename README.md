d29af0de2eff9d749b7016aa36dc7931b2402a61
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2017-10-06 10:14:24 +0700

    Update Compiler script
    Update Compiler Script and Do Some Changes

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# pak192indo
-A Repository about Simutrans Indonesian Pakset
+# PAK192.INDO Repos
+
+## 1. About
+
+Thanks for downloading this Unfinished pakset. :)
+
+This is derivation works from pak192.comic pakset for simutrans.
+We have contacted Leartin and Give us permisson to use some pak192.comic object
+for this pakset ( almost all object we use ) with some note :
+1. Licensed same as pakset  192.comic, CC-BY-SA
+2. Maintain the original "thanks.txt" "readme.txt" and Other,
+3. And some Notes i cant translate it to english.
+
+I think this pakset suitable for Railfans and Busmania Players,
+Because We Have Many Vehicle Object about Trains and Buses :D
+We Still Working for the building Addons
+
+I Configure the pakset to match Indonesian players style.
+I Using Simutrans Nightly
+
+Note :
+1. Vehicle and Building Cost Are Funny
+2. Our Colouring is Bad
+3. Our Ages Betwwen 12 - 16
+4. I Don't Know how to create "ground.outside.pak"
+5. The translation started from Scratch!
+6. DONT TRY THE SCENARIO.. Its BUGGED!!
+7. We are Using almost All of pak192.comic Object
+8. Run with simutrans Nightly Build
+9. Just Tell Me If Something Goes Wrong
+10. Thanks For Download
+
+## 2. Contact Us ?
+
+If something goes wrong with this pakset just Tell us and we will fix it
+
+Facebook : 
+Simutrans Indonesia - SMI 
+( Check out the link on simutrans.com ) *Peace
+
+
+## 3. Bug Report ?
+
+Still in Development
+
+## 4. Copyright & Licence
+
+Because this is derivation works from pak192.comic,
+So This pakset will be launched using CC-BY-SA License
+
+Some graphics their have own licenses
+
+## 5. Maintainer
+
+Rengga Prakoso N.
+[Rengga Prakoso Facebook](http://www.facebook.com/vzrenggamani)
+[Rengga Prakoso Email](mailto:rengga.prakoso@gmail.com)
+
+## 6. Thanks and Team
+
+All of Simutrans Developer Team
+
+Moderators and Members of Simutrans International Forums
+
+PAK192.COMIC Teams. 
+
+Indonesian Simutrans Players and Developer
+
+And You Of Course.
+
+All Of You <3
+
+This document was created by vzrenggamani 6.07.2016, last edited by vzrenggamani 05.10.2017
--- pak192_compiler.bat
+++ pak192_compiler.bat
@@ -15,116 +15,144 @@ attrib +H rar.exe
 attrib +R +H master
 attrib +R +H developerdoc
 attrib +R +H release
-if not exist .\makeobj.exe goto abort
+if not exist .\makeobj.exe (
 
-rem delete old exported data
-rem  ------------------------
-del \release\export\192indo.zip
-del release\*.pak
-del release\config\*.tab
-del release\text\*.tab
-del release\text\*.txt
-del release\text\*.zip
-del release\doc\*.txt
-del release\sound\*.wav
-del release\sound\*.tab
-rmdir /Q /S scenario\
-rmdir /Q /S text\
-rmdir /Q /S doc\
-rmdir /Q /S sound\
-ping 127.0.0.1 >>null
-goto makedir
+    echo makeobj.exe doesn't exist
+    echo PLEASE DOWNLOAD IT!
+    ping 127.0.0.1 >>null
+    exit
 
-:makedir
-cls.
-cd release\
-mkdir config\
-mkdir text\
-mkdir doc\
-mkdir sound\
-mkdir log\
-cd ..
-goto compile
+) else (
+    rem delete old exported data
+    rem  ------------------------
+    del .\pak192.indo.rar
+    del release\*.pak
+    del release\config\*.tab
+    del release\text\*.tab
+    del release\text\*.txt
+    del release\text\*.zip
+    del release\doc\*.txt
+    del release\sound\*.wav
+    del release\sound\*.tab
+    del release\log\*.* /Q
+    rmdir /Q /S scenario\
+    rmdir /Q /S text\
+    rmdir /Q /S doc\
+    rmdir /Q /S sound\
+    if exist .\pak192.indo.rar (
+        goto del
+    ) else (
 
-:compile
-cls.
-rem COMPILING 
-echo Compiling Objects
-makeobj DEBUG pak192 release/ master/Building/*.dat >>release/log/building.log
+        :makedir
+        cls.
+        cd release\
+        mkdir config\
+        mkdir text\
+        mkdir doc\
+        mkdir sound\
+        mkdir log\
+        cd ..
+        goto compile
 
-rem COMPILING OBJECT
-echo Compiling Misc Object
-makeobj DEBUG pak192 release/ master/goods/*.dat >>release/log/goods.log
-makeobj DEBUG pak192 release/ master/Misc/misc.dat >>release/log/misc.log
-makeobj DEBUG pak128 release/ master/Misc/logo.dat >>release/log/logo.log
-makeobj DEBUG pak192 release/ master/Misc/halfheight/*.dat >>release/log/halfheight.log
+        :compile
+        cls.
+        if not exist release/log/building.log (
+            :co_obj
+            rem COMPILING 
+            echo Compiling Objects
+            makeobj DEBUG pak192 release/ master/Building/*.dat >>release/log/building.log
+            goto compile
+        ) else (
+            if not exist release/log/goods.log (
+                :co_mobj
+                rem COMPILING OBJECT
+                echo Compiling Misc Object
+                makeobj DEBUG pak192 release/ master/goods/*.dat >>release/log/goods.log
+                makeobj DEBUG pak192 release/ master/Misc/misc.dat >>release/log/misc.log
+                makeobj DEBUG pak128 release/ master/Misc/logo.dat >>release/log/logo.log
+                makeobj DEBUG pak192 release/ master/Misc/halfheight/*.dat >>release/log/halfheight.log
+                goto compile
+            ) else (
+                if not exist release/log/signals.log (
+                    :co_sig
+                    rem COMPILING SIGNALS
+                    echo Compiling Signals
+                    makeobj DEBUG pak192 release/ master/signal/*.dat >>release/log/signals.log
+                    goto compile
+                ) else (
+                    if not exist release/log/way.log (
+                        :co_way
+                        rem COMPILING WAYS!!!!
+                        echo Compiling Ways
+                        makeobj DEBUG pak192 release/ master/way/*.dat >>release/log/way.log
+                        makeobj DEBUG pak192 release/ master/way/road/*.dat >>release/log/way.log
+                        makeobj DEBUG pak192 release/ master/way/track/*.dat >>release/log/way.log
+                        makeobj DEBUG pak192 release/ master/way/bridges/*.dat >>release/log/way.log
+                        goto compile
+                    ) else (
+                        if not exist release/log/symbol.log (
+                            :co_sym
+                            rem COMPILING SYMBOL
+                            echo Compling Symbol
+                            makeobj DEBUG pak192 release/ master/symbol/*.dat >>release/log/symbol.log
+                            goto compile
+                        ) else (
+                            if not exist release/log/vehicle.log (
+                                :co_va
+                                rem COMPILING VEHICLE
+                                echo Compiling Vehicle
+                                makeobj DEBUG pak192 release/ master/vehicle/*.dat >>release/log/vehicle.log
+                                goto compile
+                            )
+                        )
+                            
+                    )
+                    
+                )
+                
+            )
+        )
 
-rem COMPILING SIGNALS
-echo Compiling Signals
-makeobj DEBUG pak192 release/ master/signal/*.dat >>release/log/signals.log
+        :copy
+        rar.exe a master\scenario\scenario.rar master\scenario\*
+        copy /b master\maintained_object\*.* release\
+        copy /b master\scenario\scenario.rar release\scenario
+        copy /b master\doc\*.* release\doc
+        copy /b master\text\*.* release\text
+        copy /b master\config\*.* release\config
+        goto zip
 
-rem COMPILING SYMBOL
-echo Compling Symbol
-makeobj DEBUG pak192 release/ master/symbol/*.dat >>release/log/symbol.log
+        :zip
+        if not exist pak192.indo.rar (
+            rar.exe a pak192.indo.rar release\config\*
+            rar.exe a pak192.indo.rar release\sound\*
+            rar.exe a pak192.indo.rar release\scenario\*
+            rar.exe a pak192.indo.rar release\text\*
+            rar.exe a pak192.indo.rar release\doc\*
+            rar.exe a pak192.indo.rar release\*
+            goto copy
+        ) else (
+            dir /d /s
+            attrib -H makeobj.exe
+            attrib -H rar.exe
+            attrib -H -R master
+            attrib -R -H developerdoc
+            attrib -R -H release
+            del null
+            cls.
+            echo ==============
+            echo Compile Pak192.indo COMPLETE
+            echo ==============
+            echo.
 
-rem COMPILING VEHICLE
-echo Compiling Vehicle
-makeobj DEBUG pak192 release/ master/vehicle/*.dat >>release/log/vehicle.log
+            echo Compiling DONE!
+            echo Press Anykey to exit
+            pause
+            exit
+        )
 
-rem COMPILING WAYS!!!!
-echo Compiling Ways
-makeobj DEBUG pak192 release/ master/way/*.dat >>release/log/way.log
-makeobj DEBUG pak192 release/ master/way/road/*.dat >>release/log/way.log
-makeobj DEBUG pak192 release/ master/way/track/*.dat >>release/log/way.log
-makeobj DEBUG pak192 release/ master/way/bridges/*.dat >>release/log/way.log
-goto copy
+        
+    )
 
-:copy
-rar a master\scenario\scenario.rar master\scenario\*
-copy /b master\maintained_object\*.* release\
-copy /b master\scenario\scenario.rar release\scenario
-copy /b master\doc\*.* release\doc
-copy /b master\text\*.* release\text
-copy /b master\config\*.* release\config
-goto finish
-
-:zip
-cls.
-echo ZIPPING CONFIGURATION FILES!
-start rar.exe a pak192.indo.rar release\config\*
-ping 127.0.0.1 -n 10 >>null
-echo ZIPPING CONFIGURATION SOUND FILES!
-start rar.exe a pak192.indo.rar release\sound\*
-ping 127.0.0.1 -n 10 >>null
-echo ZIPPING CONFIGURATION SCENARIO FILES!
-start rar.exe a pak192.indo.rar release\scenario\*
-ping 127.0.0.1 -n 10 >>null
-echo ZIPPING CONFIGURATION TEXT FILES!
-start rar.exe a pak192.indo.rar release\text\*
-ping 127.0.0.1 -n 10 >>null
-echo ZIPPING CONFIGURATION DOC FILES!
-start rar.exe a pak192.indo.rar release\doc\*
-ping 127.0.0.1 -n 10 >>null
-echo ZIPPING CONFIGURATION pak FILES!
-start rar.exe a pak192.indo.rar release\*
-
-
-
-:finish
-attrib -H makeobj.exe
-attrib -H rar.exe
-attrib -H -R master
-attrib -R -H developerdoc
-attrib -R -H release
-del null
-cls.
-echo ==============
-echo Compile Pak192.indo COMPLETE
-echo ==============
-echo.
-
-echo Compiling DONE!
-echo Press Anykey to exit
-pause
-start .\release\
-exit
+    
+)
